6b3d1bcb41105204fa805c593de871dbe2da154a
Joe Durand <joe.durand3@gmail.com>
2022-07-24 16:45:28 -0500

    More tweaks

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-west-1"
 
   default_tags {
     tags = {
