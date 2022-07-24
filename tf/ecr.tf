b703af092bf6481e590b725dbd57473c380f3f75
Joe Durand <joe.durand3@gmail.com>
2022-07-24 16:18:45 -0500

    Remove environment variable

--- tf/ecr.tf
+++ tf/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "joe-sandbox" {
-  name                 = "application-jsandbox-${var.ENVIRONMENT}"
+  name                 = "application-jsandbox"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
